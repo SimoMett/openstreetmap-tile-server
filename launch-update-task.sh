9d5200e25d7272026ce3824b38b3ed7c1938fde1
SimoMett <matt.simonetti@hotmail.it>
2023-10-12 12:10:46 +0000

    Fixed bug not getting container id

--- launch-update-task.sh
+++ launch-update-task.sh
@@ -1,1 +1,1 @@
-docker exec -it `docker ps -qf ancestor=openstreetmap-tile-server_map` bash autoimport-updates.sh updates.osc.gz
+docker exec -it `docker ps -qf ancestor=openstreetmap-tile-server-map` bash autoimport-updates.sh updates.osc.gz
