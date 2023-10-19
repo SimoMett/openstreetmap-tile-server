a0aaefe211ad2d1d5ce7f8e96e5de613e3b45962
SimoMett <matt.simonetti@hotmail.it>
2023-10-19 14:45:57 +0000

    Update launch-update-task.sh

--- launch-update-task.sh
+++ launch-update-task.sh
@@ -1,1 +1,3 @@
+echo "[launch-update-task.sh] se questo comando fallisce provare con"
+echo "docker exec -it \`docker ps -qf ancestor=openstreetmap-tile-server_map\` bash autoimport-updates.sh updates.osc.gz"
 docker exec -it `docker ps -qf ancestor=openstreetmap-tile-server-map` bash autoimport-updates.sh updates.osc.gz
