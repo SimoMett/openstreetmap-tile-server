bb02776a98cf60ef928159d2994e4d8ee9072fff
SimoMett <matt.simonetti@hotmail.it>
2023-10-25 10:08:12 +0200

    New strategy to find container id

--- launch-update-task.sh
+++ launch-update-task.sh
@@ -1,3 +1,14 @@
-echo "[launch-update-task.sh] se questo comando fallisce provare con"
-echo "docker exec -it \`docker ps -qf ancestor=openstreetmap-tile-server_map\` bash autoimport-updates.sh updates.osc.gz"
-docker exec -it `docker ps -qf ancestor=openstreetmap-tile-server-map` bash autoimport-updates.sh updates.osc.gz
+TILE_SERVER_CONTAINER=$(docker ps -qf ancestor=openstreetmap-tile-server_map)
+
+if [ -z "$TILE_SERVER_CONTAINER" ] ; then
+	TILE_SERVER_CONTAINER=$(docker ps -qf ancestor=openstreetmap-tile-server-map)
+fi
+
+if [ -z "$TILE_SERVER_CONTAINER" ] ; then
+      echo "[launch-update-task.sh] non è stato possibile reperire l'id del container nel tile server. Rivedere questo script"
+      exit 1
+fi
+
+echo $TILE_SERVER_CONTAINER
+exit 0
+docker exec -it $TILE_SERVER_CONTAINER bash autoimport-updates.sh updates.osc.gz
